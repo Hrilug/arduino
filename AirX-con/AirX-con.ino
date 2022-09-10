a98af6a0245fa542d42eac61cd6522f0b72e5f7a
Hrilug <3198727736@qq.com>
2022-09-10 12:21:35 +0800

    2022-9-10-1

--- AirX-con/AirX-con.ino
+++ AirX-con/AirX-con.ino
@@ -21,7 +21,7 @@ Adafruit_SSD1306 display(128, 64, &Wire, -1);
 int gun_v,x_v,y_v,l_v,r_v,k1_v,k2_v,k3_v,k4_v,k5_v,k6_v=0;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println();
   Serial.begin("Hello,It's AirX!");
 
@@ -49,36 +49,43 @@ void setup() {
   delay(1000);
   display.setTextSize(2);
   display.setCursor(0, 30);
-  display.println("AirX");
+  display.println("AirX!");
   display.display();
   delay(3000);
   display.clearDisplay();
-  display.display();
   delay(500);
-}
 
-int analyze(int n,int v){
-  if n != v{
-    v=n;
-  display.setTextSize(2);
-  display.setCursor(0,0);
-  display.println(b);
-  display.display();
-  }
-  return v;
 }
 
 void loop() {
-  gun_v=analyze(analogRead(gun),gun_v);
-  x_v=analyze(analogRead(xaxis),x_v);
-  y_v=analyze(analogRead(yaxis),y_v);
-  l_v=analyze(digitalWrite(left),l_v);
-  r_v=analyze(digitalWrite(right),r_v);
-  k1_v=analyze(digitalWrite(key1),k1_v);
-  k2_v=analyze(digitalWrite(key2),k2_v);
-  k3_v=analyze(digitalWrite(key3),k3_v);
-  k4_v=analyze(digitalWrite(key4),k4_v);
-  k5_v=analyze(digitalWrite(key5),k5_v);
-  k6_v=analyze(digitalWrite(key6),k6_v);
+  gun_v=analogRead(gun);
+  x_v=analogRead(xaxis);
+  y_v=analogRead(yaxis);
+  l_v=digitalRead(left);
+  r_v=digitalRead(right);
+  k1_v=digitalRead(key1);
+  k2_v=digitalRead(key2);
+  k3_v=digitalRead(key3);
+  k4_v=digitalRead(key4);
+  k5_v=digitalRead(key5);
+  k6_v=digitalRead(key6);
+
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setCursor(0, 5);
+  display.println("gun:");
+  display.setCursor(0, 25);
+  display.println("x-a:");
+  display.setCursor(0, 45);
+  display.println("y-a:");
+  display.setCursor(40, 5);
+  display.println(gun_v);
+  display.setCursor(40, 25);
+  display.println(x_v);
+  display.setCursor(40, 45);
+  display.println(y_v);
+  display.display();
+  delay(500);
+  
   
 }

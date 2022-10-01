e7bda2329753f1d3a1b3694a55a3975519412245
Hrilug <3198727736@qq.com>
2022-10-01 12:33:33 +0800

    2022-10-1-1

--- OLED-Shield64x48/OLED-Shield64x48/OLED-Shield64x48.ino
+++ OLED-Shield64x48/OLED-Shield64x48/OLED-Shield64x48.ino
@@ -7,7 +7,7 @@
 Adafruit_SSD1306 display(OLED_RESET);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //or 0x3C
   display.clearDisplay();
